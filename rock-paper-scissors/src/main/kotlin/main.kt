e7f5d89f94ffdec8fc29bacfc2fce7317502eb9f
CosimaLagae <cosima.lagae@student.ehb.be>
2021-10-05 17:39:44 +0200

    finish

--- rock-paper-scissors/src/main/kotlin/main.kt
+++ rock-paper-scissors/src/main/kotlin/main.kt
@@ -1,38 +1,16 @@
-/*fun main(){
-    val names = arrayOf("rock", "paper", "scissor",)
-    val randomElement = names.random()    // here we can use the selected element to print it for example
-    println(randomElement)
-}*/
-
-import java.util.Scanner
-
 fun main(args: Array<String>) {
     val names = arrayOf("rock", "paper", "scissor",)
-    val randomElement = names.random()    // here we can use the selected element to print it for example
-    println(randomElement)
-
-
-    // Creates an instance which takes input from standard input (keyboard)
-    val reader = Scanner(System.`in`)
-    print("Enter your choice: ")
-
-    // nextInt() reads the next integer from the keyboard
-    var integer:Int = reader.nextInt()
-
-    println("my choice: $randomElement")
+    val computerChoice = names.random()    // here we can use the selected element to print it for example
 
 
     println("your choice: ")
-    val userChoice
+    val userChoice = readLine()!!
+    println(computerChoice)
+    determineWinner(computerChoice, userChoice)
+    println("computer choice was: $computerChoice")
 }
 
 
-fun getComputerChoice(){
-
-}
-fun getUserChoice(options: Array<String>){
-
-}
 fun determineWinner(computerChoice: String, userChoice: String?){
     if (computerChoice == userChoice){
         println("it's a draw")
