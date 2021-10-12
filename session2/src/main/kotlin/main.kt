99e280741205ededcc9617ff7b10d44a00943002
CosimaLagae <cosima.lagae@student.ehb.be>
2021-10-12 15:06:04 +0200

    intro made

---
+++ session2/src/main/kotlin/main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("welcome to the climate game")
+    challengeOne()
+}
+
+fun challengeOne(){
+    println("In the first challenge you need to solve my riddle")
+    println("")
+    println("")
+
+}
+
+fun challengeTwo(){
+
+}
