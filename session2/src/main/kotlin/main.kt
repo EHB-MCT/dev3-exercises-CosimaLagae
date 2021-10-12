a6bfe7ac57a8f969ac7f63649afa907d466399b5
CosimaLagae <cosima.lagae@student.ehb.be>
2021-10-12 15:10:43 +0200

    riddle was added

--- session2/src/main/kotlin/main.kt
+++ session2/src/main/kotlin/main.kt
@@ -4,10 +4,11 @@ fun main() {
 }
 
 fun challengeOne(){
-    println("In the first challenge you need to solve my riddle")
-    println("")
-    println("")
+    println("In the first challenge you need to solve my riddle.")
+    println("my riddle is:")
+    println("What does the weatherman always say to a leprechaun?")
 
+    var correctAnswer  setOf
 }
 
 fun challengeTwo(){
