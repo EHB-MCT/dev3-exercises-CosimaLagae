98e691a255fda8735760b263d73576304d82362f
CosimaLagae <cosima.lagae@student.ehb.be>
2021-10-12 16:31:39 +0200

    Wip challenge Two

--- session2/src/main/kotlin/main.kt
+++ session2/src/main/kotlin/main.kt
@@ -20,9 +20,41 @@ fun challengeOne(){
 }
 
 fun challengeTwo(){
+    println(" ")
+    println("welcome to Challenge Two")
+    println("You need to guess which number the dice will stop on")
+    println("pick: low (1-3) or high (4-6)")
 
+    val userAnswer = readLine()
+    val diceNumber = (1..6).random()
+
+    if (userAnswer == "low") {
+        val diceNum = if (diceNumber > 3) high else low
+    } else {
+        
+    }
+
+    val low = arrayOf(1,2,3)
+    val high = arrayOf(4,5,6)
+
+    val diceNumber = (1..6).random()
+
+    /*when (diceNumber) {
+        1,2,3 -> if (userAnswer == low)
+    }*/
 }
 
+
+fun challengeThree(){
+    println(" ")
+    println("welcome to Challenge Three")
+    println("You need to choose your object of choice, if you choose the wrong one, you will be redirected")
+    println("objects: a flower, the sun or a tree")
+
+
+
+
+}
 fun gameOver(){
  println("Game Over")
 }
