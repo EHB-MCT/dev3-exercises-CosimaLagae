229432f8eaec4eff0b2890892f79244526d970a2
CosimaLagae <cosima.lagae@student.ehb.be>
2021-11-23 15:52:36 +0100

    cred fix2

--- session6/src/main/kotlin/Main.kt
+++ session6/src/main/kotlin/Main.kt
@@ -8,15 +8,15 @@ fun main(){
 
     // Prepare credentials
     val connectionProps = Properties()
-    connectionProps["user"] = credentials.databaseName
-    connectionProps["password"] = credentials.databaseName
+    connectionProps["user"] = credentials.user
+    connectionProps["password"] = credentials.password
 
 
 // Create the connection: this will allow us to run queries on it later
     val connection =  DriverManager.getConnection(
         "jdbc:" + "mysql" + "://" +
                 "dt5.ehb.be" +
-                ":" + "3306" + "/" +"2122DEV3034",
+                ":" + "3306" + "/" +credentials.databaseName,
         connectionProps)
 
 }
